4a800da71a574b426ee1ee88b5fff6bf9eb338b0
BK <qwerty0911@gachon.ac.kr>
2026-09-01 01:16:09 +0900

    day1 피로도

---
+++ 피로도.py
@@ -0,0 +1,23 @@
+
+
+def solution(k, dungeons):
+    answer = 0
+    
+    visited = [False] * len(dungeons)
+    
+    def dfs(k, count):
+        nonlocal answer
+
+        answer = max(answer , count)
+
+        for i in range(len(visited)):
+            if visited[i] == False and k >= dungeons[i][0]:
+                visited[i] = True
+                dfs(k-dungeons[i][1], count + 1)
+                visited[i] = False
+
+    dfs(k, 0)
+    
+    return answer
+
+print(solution(80,[[80,20],[50,40],[30,10]]))
